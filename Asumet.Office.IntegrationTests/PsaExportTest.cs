9e747f4fc0013d228bc44e5fcd75ee51f320d0bc
elshev <elshev@gmail.com>
2023-10-18 15:35:26 +0400

    PsaExportTest.cs: Improve tests

--- Asumet.Office.IntegrationTests/PsaExportTest.cs
+++ Asumet.Office.IntegrationTests/PsaExportTest.cs
@@ -1,5 +1,6 @@
 ﻿using Asumet.Models;
 using Microsoft.Extensions.Configuration;
+using NPOI.XWPF.UserModel;
 
 namespace Asumet.Office.IntegrationTests
 {
@@ -15,6 +16,34 @@ namespace Asumet.Office.IntegrationTests
             AppSettings.Instance.UpdateConfiguration(configuration);
         }
 
+        private static void AssertDocxFile(string filePath)
+        {
+            filePath.Should().NotBeNull();
+            File.Exists(filePath).Should().BeTrue();
+            var fileName = Path.GetFileName(filePath);
+            fileName.StartsWith("ПСА").Should().BeTrue();
+            var fileExtension = Path.GetExtension(filePath);
+            fileExtension.Should().Be(".docx");
+        }
+
+        private static bool AreTextsExistInDoc(XWPFDocument doc, IEnumerable<string> stringsToFind)
+        {
+            var found = stringsToFind.ToDictionary(s => s, s => false);
+            foreach (var paragraph in doc.Paragraphs)
+            {
+                foreach (var s in found.Keys)
+                {
+                    found[s] = found[s] || paragraph.ParagraphText.Contains(s);
+                }
+                if (found.All(kvp => kvp.Value))
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
         [Fact]
         public void TestExportPsa()
         {
@@ -27,12 +56,19 @@ namespace Asumet.Office.IntegrationTests
 
             // Assert
             var filePath = psaExporter.OutputFilePath;
-            filePath.Should().NotBeNull();
-            File.Exists(filePath).Should().BeTrue();
-            var fileName = Path.GetFileName(filePath);
-            fileName.StartsWith("ПСА").Should().BeTrue();
-            var fileExtension = Path.GetExtension(filePath);
-            fileExtension.Should().Be(".docx");
+            AssertDocxFile(filePath);
+
+            using var rs = File.OpenRead(filePath);
+            using var doc = new XWPFDocument(rs);
+            foreach (var paragraph in doc.Paragraphs)
+            {
+            }
+
+            var table = doc.Tables.Where(t => t.NumberOfRows > 0).FirstOrDefault();
+            Assert.NotNull(table);
+            table.Rows.Count.Should().BeGreaterThan(3);
+            AreTextsExistInDoc(doc, new string[] { psa.ActNumber??"", psa.Buyer?.FullName??"", psa.Supplier?.FullName??"" })
+                .Should().BeTrue();
         }
     }
 }
